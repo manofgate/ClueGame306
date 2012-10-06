f97141ffbbeb6b81b032e7ead1f499550ead0928
Matt Jordan <mjordan@mines.edu>
2012-10-05 22:06:11 -0600

    Added the other tests we needed for the assignment.

--- Clue2/src/tests/IntBoardTests.java
+++ Clue2/src/tests/IntBoardTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import java.util.LinkedList;
+import java.util.TreeSet;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -55,6 +56,113 @@ public class IntBoardTests {
 		Assert.assertTrue(adjList.contains(11));
 		Assert.assertTrue(adjList.contains(6));
 		Assert.assertEquals(3, adjList.size());
+	}
+	
+	@Test
+	public void testAdjacency4(){
+		//test the adjacency list for the right edge
+		LinkedList adjList = board.getAdjList(8);
+		Assert.assertTrue(adjList.contains(4));
+		Assert.assertTrue(adjList.contains(9));
+		Assert.assertTrue(adjList.contains(12));
+		Assert.assertEquals(3, adjList.size());
+	}
+	
+	@Test
+	public void testAdjacency5() {
+		//test the adjacency list for the right edge
+		LinkedList adjList = board.getAdjList(5);
+		Assert.assertTrue(adjList.contains(1));
+		Assert.assertTrue(adjList.contains(4));
+		Assert.assertTrue(adjList.contains(6));
+		Assert.assertTrue(adjList.contains(9));
+		Assert.assertEquals(4, adjList.size());
+	}
+	
+	@Test
+	public void testAdjacency6() {
+		//test the adjacency list for the right edge
+		LinkedList adjList = board.getAdjList(10);
+		Assert.assertTrue(adjList.contains(6));
+		Assert.assertTrue(adjList.contains(9));
+		Assert.assertTrue(adjList.contains(11));
+		Assert.assertTrue(adjList.contains(14));
+		Assert.assertEquals(4, adjList.size());
+	}
+	
+	@Test
+	public void testPath1() {
+		board.calcTargets(0, 3);
+		TreeSet targets= board.getTargets();
+		Assert.assertEquals(6, targets.size());
+		Assert.assertTrue(targets.contains(12));
+		Assert.assertTrue(targets.contains(9));
+		Assert.assertTrue(targets.contains(1));
+		Assert.assertTrue(targets.contains(6));
+		Assert.assertTrue(targets.contains(3));
+		Assert.assertTrue(targets.contains(4));
+	}
+	
+	@Test
+	public void testPath2() {
+		board.calcTargets(15, 3);
+		TreeSet targets= board.getTargets();
+		Assert.assertEquals(6, targets.size());
+		Assert.assertTrue(targets.contains(12));
+		Assert.assertTrue(targets.contains(9));
+		Assert.assertTrue(targets.contains(6));
+		Assert.assertTrue(targets.contains(3));
+		Assert.assertTrue(targets.contains(11));
+		Assert.assertTrue(targets.contains(14));
+	}
+	
+	@Test
+	public void testPath3() {
+		board.calcTargets(3, 3);
+		TreeSet targets= board.getTargets();
+		Assert.assertEquals(6, targets.size());
+		Assert.assertTrue(targets.contains(0));
+		Assert.assertTrue(targets.contains(5));
+		Assert.assertTrue(targets.contains(10));
+		Assert.assertTrue(targets.contains(15));
+		Assert.assertTrue(targets.contains(2));
+		Assert.assertTrue(targets.contains(7));
+	}
+	
+	@Test
+	public void testPath4() {
+		board.calcTargets(12, 3);
+		TreeSet targets= board.getTargets();
+		Assert.assertEquals(6, targets.size());
+		Assert.assertTrue(targets.contains(0));
+		Assert.assertTrue(targets.contains(5));
+		Assert.assertTrue(targets.contains(10));
+		Assert.assertTrue(targets.contains(15));
+		Assert.assertTrue(targets.contains(8));
+		Assert.assertTrue(targets.contains(13));
+	}
+	
+	@Test
+	public void testPath5() {
+		board.calcTargets(5, 2);
+		TreeSet targets= board.getTargets();
+		Assert.assertEquals(6, targets.size());
+		Assert.assertTrue(targets.contains(0));
+		Assert.assertTrue(targets.contains(10));
+		Assert.assertTrue(targets.contains(8));
+		Assert.assertTrue(targets.contains(13));
+		Assert.assertTrue(targets.contains(7));
+		Assert.assertTrue(targets.contains(5));
+	}
+	
+	@Test
+	public void testPath6() {
+		board.calcTargets(7, 1);
+		TreeSet targets= board.getTargets();
+		Assert.assertEquals(3, targets.size());
+		Assert.assertTrue(targets.contains(3));
+		Assert.assertTrue(targets.contains(6));
+		Assert.assertTrue(targets.contains(11));
 	}
 	
 }
