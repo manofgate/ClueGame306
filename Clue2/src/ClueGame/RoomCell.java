3bc35b0bd0151bbae2c1a6d554bc433f32d19398
Matthew Jordan <mjordan@mymail.mines.edu>
2012-10-11 22:51:21 -0600

    Finished tests.

--- Clue2/src/ClueGame/RoomCell.java
+++ Clue2/src/ClueGame/RoomCell.java
@@ -5,7 +5,20 @@ public class RoomCell extends BoardCell {
 		UP, DOWN, LEFT, RIGHT, NONE;
 	}
 	private DoorDirection doorDirection;
+	private char roomInitial;
 	
-	public boolean isRomm(){return true;}
+	public boolean isRoom(){return true;}
 	//private void draw(){};
+
+	public DoorDirection getDoorDirection() {
+		return doorDirection;
+	}
+
+	public void setDoorDirection(DoorDirection doorDirection) {
+		this.doorDirection = doorDirection;
+	}
+
+	public char getRoomInitial() {
+		return roomInitial;
+	}
 }
--- Clue2/src/clueTests/BoardInitTests.java
+++ Clue2/src/clueTests/BoardInitTests.java
@@ -1,14 +1,96 @@
 package clueTests;
 
-import static org.junit.Assert.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+import junit.framework.Assert;
+
+import org.junit.Before;
 import org.junit.Test;
 
-public class BoardInitTests {
+import ClueGame.Board;
+import ClueGame.BoardCell;
+import ClueGame.RoomCell;
+import ClueGame.RoomCell.DoorDirection;
 
+public class BoardInitTests {
+	
+	Board gameBoard;
+
+	@Before
+	public void setup() {
+		gameBoard = new Board();
+		gameBoard.loadConfigFiles();
+		gameBoard.loadLegend();
+		gameBoard.loadBoard();
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testRoomCount() {
+		Assert.assertEquals(gameBoard.getRooms().size(), 9);
+	}
+	
+	@Test
+	public void testCharacterMapping() {
+		Assert.assertEquals(gameBoard.getRooms().get('W'), "Walkway");
+		Assert.assertEquals(gameBoard.getRooms().get('1'), "Conservatory");
+		Assert.assertEquals(gameBoard.getRooms().get('2'), "Indoor Pool");
+		Assert.assertEquals(gameBoard.getRooms().get('3'), "Kitchen");
+		Assert.assertEquals(gameBoard.getRooms().get('4'), "Study");
+		Assert.assertEquals(gameBoard.getRooms().get('5'), "Dining Room");
+		Assert.assertEquals(gameBoard.getRooms().get('6'), "Living Room");
+		Assert.assertEquals(gameBoard.getRooms().get('7'), "Entryway");
+		Assert.assertEquals(gameBoard.getRooms().get('8'), "Library");
+		Assert.assertEquals(gameBoard.getRooms().get('9'), "Tower");
+	}
+	
+	@Test
+	public void testNumRows() {
+		Assert.assertEquals(gameBoard.getNumRows(), 16);
+	}
+	
+	@Test
+	public void testNumCols() {
+		Assert.assertEquals(gameBoard.getNumColumns(), 21);
+	}
+	
+	@Test
+	public void testDoors() {
+		Assert.assertEquals(gameBoard.getRoomCellAt(3, 2).isDoorway(), true);
+		Assert.assertEquals(gameBoard.getRoomCellAt(3, 2).getDoorDirection(), DoorDirection.DOWN);
+		Assert.assertEquals(gameBoard.getRoomCellAt(2, 2).isDoorway(), false);
+	}
+	
+	@Test
+	public void testNumberRecognizedRooms() {
+		Set<RoomCell> rooms = new HashSet<RoomCell>();
+		List<BoardCell> board = gameBoard.getCells();
+		for(BoardCell bc : board) {
+			if(bc.isRoom()) {
+				rooms.add((RoomCell) bc);
+			}
+		}
+		Assert.assertEquals(rooms.size(), board.size());
+	}
+	
+	@Test
+	public void testRoomInitials() {
+		Assert.assertEquals(gameBoard.getRoomCellAt(2, 2).getRoomInitial(), '1');
+		Assert.assertEquals(gameBoard.getRoomCellAt(2, 18).getRoomInitial(), '2');
+		Assert.assertEquals(gameBoard.getRoomCellAt(0, 38).getRoomInitial(), '3');
+		Assert.assertEquals(gameBoard.getRoomCellAt(8, 6).getRoomInitial(), '4');
+		Assert.assertEquals(gameBoard.getRoomCellAt(7, 30).getRoomInitial(), '5');
+		Assert.assertEquals(gameBoard.getRoomCellAt(13, 10).getRoomInitial(), '6');
+		Assert.assertEquals(gameBoard.getRoomCellAt(14, 22).getRoomInitial(), '7');
+		Assert.assertEquals(gameBoard.getRoomCellAt(15, 40).getRoomInitial(), '8');
+		Assert.assertEquals(gameBoard.getRoomCellAt(7, 20).getRoomInitial(), '9');
+	}
+	
+	@Test
+	public void testCalcIndex() {
+		Assert.assertEquals(gameBoard.calcIndex(0, 0), 0);
+		Assert.assertEquals(gameBoard.calcIndex(1, 0), gameBoard.getNumColumns());
 	}
 
 }
