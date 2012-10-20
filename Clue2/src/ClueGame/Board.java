2f786ec955c1f25995b9c3a4b8903d0778d22219
Matt Jordan <mjordan@mines.edu>
2012-10-19 23:34:43 -0600

    Formatted the code in Board. If you want to format quickly, ctrl-shift-f.

--- Clue2/src/ClueGame/Board.java
+++ Clue2/src/ClueGame/Board.java
@@ -10,204 +10,201 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class Board {
+
 	private ArrayList<BoardCell> cells = new ArrayList<BoardCell>();
 	private Map<Character, String> rooms = new HashMap<Character, String>();
 	private int numRows;
 	private int numColumns;
-	public void loadConfigFiles(){
+
+	private static int GRID_PIECES;
+	private Map<Integer, LinkedList<Integer>> adjMtx = new HashMap<Integer, LinkedList<Integer>>();
+	private Set<BoardCell> targets = new HashSet<BoardCell>();
+
+	public void loadConfigFiles() {
 		loadLegend();
 		loadBoard();
 	}
-	public void loadLegend(){
-		try{
-			
+
+	private void loadLegend() {
+		try {
+
 			FileReader read = new FileReader("legendBoard.txt");
 			Scanner scan = new Scanner(read);
 			String in = "";
 			String name = "";
 			int i = 0;
-			while(scan.hasNextLine()){
+			while (scan.hasNextLine()) {
 				i++;
 				in = scan.nextLine();
 				String[] vars = in.split(", ");
 				name = vars[1].substring(0);
 				char n = vars[0].charAt(0);
-				if(n!= 'W')
+				if (n != 'W')
 					rooms.put(n, name);
 			}
 			scan.close();
-			//Set<Character> Key = rooms.keySet();
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.getStackTrace();
 		}
 	}
-	public void loadBoard(){
-		try{
-			int row =0;
+
+	private void loadBoard() {
+		try {
+			int row = 0;
 			int col = 0;
 			FileReader reader = new FileReader("Board.txt");
 			Scanner scan = new Scanner(reader);
 			String part = "";
-			while(scan.hasNextLine()){
+			while (scan.hasNextLine()) {
 				row++;
 				part = scan.nextLine();
 				String[] vars = part.split(",");
-				for(String str: vars){
-					if(row<=1)
+				for (String str : vars) {
+					if (row <= 1)
 						col++;
-					if(str.equals("W")){
+					if (str.equals("W")) {
 						WalkwayCell c = new WalkwayCell();
 						cells.add(c);
-					}
-					else{
+					} else {
 						RoomCell c = new RoomCell(str);
 						cells.add(c);
 					}
 				}
-				if(row <=1)
+				if (row <= 1)
 					setNumColumns(col);
 			}
 			setNumRows(row);
 			scan.close();
-			GRID_PIECES = numRows*numColumns;
-		}catch(Exception e){
+			GRID_PIECES = numRows * numColumns;
+		} catch (Exception e) {
 			e.getStackTrace();
 		}
-		
+
 	}
-	public int calcIndex(int row, int col){
-		return numColumns*row + col;
+
+	public int calcIndex(int row, int col) {
+		return numColumns * row + col;
 	}
-	public RoomCell getRoomCellAt(int row, int col){
+
+	public RoomCell getRoomCellAt(int row, int col) {
 		RoomCell roomCell = new RoomCell();
 		roomCell = (RoomCell) getCells().get(calcIndex(row, col));
 		return roomCell;
 	}
-	public Board() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+
 	public ArrayList<BoardCell> getCells() {
 		return cells;
 	}
+
 	public void setCells(ArrayList<BoardCell> cells) {
 		this.cells = cells;
 	}
+
 	public Map<Character, String> getRooms() {
 		return rooms;
 	}
+
 	public void setRooms(Map<Character, String> rooms) {
 		this.rooms = rooms;
 	}
+
 	public int getNumRows() {
 		return numRows;
 	}
+
 	public void setNumRows(int numRows) {
 		this.numRows = numRows;
 	}
+
 	public int getNumColumns() {
 		return numColumns;
 	}
+
 	public void setNumColumns(int numColumns) {
 		this.numColumns = numColumns;
 	}
-	//0 indexed
-	public LinkedList<Integer> getAdjList(int row, int col) { return getAdjList(calcIndex(row, col)); }
-	//public LinkedList<Integer> getAdjList(int cell) { return null; }
-	//public Set<Integer> calcTargets(int location, int distance) { return null; }
-	private static int GRID_PIECES;
-	private Map<Integer, LinkedList<Integer>> adjMtx =new HashMap<Integer, LinkedList<Integer>>();
-	private Set<BoardCell> targets = new HashSet<BoardCell>();
-	
-	/*public IntBoard() {
-		super();
-		// TODO Auto-generated constructor stub
-		GRID_PIECES = ROWS*COLS;
-		adjMtx = new HashMap<Integer, LinkedList<Integer>>();
-		targets = new HashSet<Integer>();
+
+	public LinkedList<Integer> getAdjList(int row, int col) {
+		return getAdjList(calcIndex(row, col));
 	}
-	*/
-	public void calcAdjacencies(){
-		//for loops
-		for(int i = 0; i<GRID_PIECES; ++i){
+
+	public void calcAdjacencies() {
+		// for loops
+		for (int i = 0; i < GRID_PIECES; ++i) {
 			adjMtx.put(i, getAdjList(i));
 		}
 	}
-	public void calcTargets(int startLoc, int steps){
+
+	public void calcTargets(int startLoc, int steps) {
 		targets.clear();
 		calcTargetsRecursion(startLoc, steps);
 	}
+
 	private void calcTargetsRecursion(int startLoc, int steps) {
-		if(steps == 0) {
+		if (steps == 0) {
 			targets.add(getCells().get(startLoc));
 			return;
 		}
-		LinkedList<Integer> start =  getAdjList(startLoc);
-		for(int i = 0; i < start.size(); i++) {
-			calcTargetsRecursion(start.get(i), steps-1);
+		LinkedList<Integer> start = getAdjList(startLoc);
+		for (int i = 0; i < start.size(); i++) {
+			calcTargetsRecursion(start.get(i), steps - 1);
 		}
 	}
-	public Set getTargets(){
+
+	public Set<BoardCell> getTargets() {
 		return targets;
 	}
-	public LinkedList<Integer> getAdjList(int cell){
+
+	public LinkedList<Integer> getAdjList(int cell) {
 		LinkedList<Integer> adjList = new LinkedList<Integer>();
-		if(cell %numColumns ==0){
-			//left edge
-			if(cell == 0){
-				//top
-				adjList.add(cell +1);
-				adjList.add(cell+numColumns);
-			}
-			else if(cell == GRID_PIECES - numColumns){
-				//bottom
-				adjList.add(cell +1);
-				adjList.add(cell-numColumns);
-			}
-			else {
+		if (cell % numColumns == 0) {
+			// left edge
+			if (cell == 0) {
+				// top
+				adjList.add(cell + 1);
+				adjList.add(cell + numColumns);
+			} else if (cell == GRID_PIECES - numColumns) {
+				// bottom
+				adjList.add(cell + 1);
+				adjList.add(cell - numColumns);
+			} else {
 				// inbetween
-				adjList.add(cell+1);
-				adjList.add(cell+numColumns);
-				adjList.add(cell-numColumns);
-			}		
-		}
-		else if((cell+1)%numColumns == GRID_PIECES%numColumns){
-			//right edge
-			if(cell == numColumns -1){
-				//top
-				adjList.add(cell -1);
-				adjList.add(cell+numColumns);
+				adjList.add(cell + 1);
+				adjList.add(cell + numColumns);
+				adjList.add(cell - numColumns);
 			}
-			else if(cell == GRID_PIECES - 1){
-				//bottom
-				adjList.add(cell -1);
-				adjList.add(cell-numColumns);
-			}
-			else {
+		} else if ((cell + 1) % numColumns == GRID_PIECES % numColumns) {
+			// right edge
+			if (cell == numColumns - 1) {
+				// top
+				adjList.add(cell - 1);
+				adjList.add(cell + numColumns);
+			} else if (cell == GRID_PIECES - 1) {
+				// bottom
+				adjList.add(cell - 1);
+				adjList.add(cell - numColumns);
+			} else {
 				// inbetween
-				adjList.add(cell-1);
-				adjList.add(cell+numColumns);
-				adjList.add(cell-numColumns);
+				adjList.add(cell - 1);
+				adjList.add(cell + numColumns);
+				adjList.add(cell - numColumns);
 			}
-		}
-		else if(cell < numColumns){
-			//on top
-			adjList.add(cell+1);
-			adjList.add(cell -1);
-			adjList.add(cell+numColumns);
-		}
-		else if(cell >(GRID_PIECES - numColumns)){
-			//on bottom
-			adjList.add(cell+1);
-			adjList.add(cell-1);
-			adjList.add(cell-numColumns);
-		}
-		else{
-			//inbetween
-			adjList.add(cell+1);
-			adjList.add(cell-1);
-			adjList.add(cell+numColumns);
-			adjList.add(cell-numColumns);
+		} else if (cell < numColumns) {
+			// on top
+			adjList.add(cell + 1);
+			adjList.add(cell - 1);
+			adjList.add(cell + numColumns);
+		} else if (cell > (GRID_PIECES - numColumns)) {
+			// on bottom
+			adjList.add(cell + 1);
+			adjList.add(cell - 1);
+			adjList.add(cell - numColumns);
+		} else {
+			// inbetween
+			adjList.add(cell + 1);
+			adjList.add(cell - 1);
+			adjList.add(cell + numColumns);
+			adjList.add(cell - numColumns);
 		}
 		return adjList;
 	}
